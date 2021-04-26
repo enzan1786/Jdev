936a9e00c2b724367fc8ccf4eb9a5f0e995b7e72
enzan1786 <46223569+enzan1786@users.noreply.github.com>
2021-04-26 20:12:10 +0700

    + initialize source

---
+++ CoffeeMintClient/src/test/java/com/green/jdev010/CoffeeMintClient/CoffeeMintClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.jdev010.CoffeeMintClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMintClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
